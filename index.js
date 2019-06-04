ca12d2f5062ef71b422773075ee5f8862df4315e
mengkko <aappcc1@icloud.com>
2019-06-04 15:50:30 +0900

    날짜계산 오류 수정

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ Date.prototype.yyyymmdd = function(){
     return yyyy +'-'+ (mm[1] ? mm + '-' : '0'+mm[0] + '-') + (dd[1] ? dd : '0'+dd[0]);
 }
 const today = (new Date()).yyyymmdd()
+const commitSection = []
 // yyyy-mm-dd 형태로 오늘을 저장
 const chain = []
 // 빈배열 생성
 let userList = userData.userList
 // ./data/userList.js에서 데이터를 import
-
 for(const i in userList) {
     client.fetch("https://github.com/"+ userList[i].gitHubName, function(err, $, res) {
         if(err) {
@@ -25,7 +25,7 @@ for(const i in userList) {
         userList[i].commit = ($(`[data-date=${today}]`)[0]['attribs']['data-count'])
         
         for(const i in $('.day')) {
-            if(i <366) {
+            if(i < $('.day').length -1) {
                 chain[i] = Number($('.day')[i]['attribs']['data-count'])
             }
         }
@@ -34,7 +34,7 @@ for(const i in userList) {
     })
 }
 log('크롤링 진행중 입니다.....(인터넷 속도가 느리면 실패할 수도 있습니다.)')
-log('node는 아직 공부중이라 동기/비동기 코드 작성이 어려워 setTimeout()로 5초뒤에 데이터 정렬합니다.')
+// log('node는 아직 공부중이라 동기/비동기 코드 작성이 어려워 setTimeout()로 10초뒤에 데이터 정렬합니다.')
 
 global.setTimeout(()=> {
     const bad = userList.filter((user) => user.commit === '0')
@@ -56,9 +56,10 @@ global.setTimeout(()=> {
     log('-----------------------------------------------')
     log('오늘 잔디 심은 사람')
     log('')
+    log(chain.length)
     for(const i in good) {
         log(`${good[i].nickName}`)
-        log(`          (연속으로 잔디 심은 날 :${365 - good[i].chain}일)`)
+        log(`          (연속으로 잔디 심은 날 :${chain.length - good[i].chain}일)`)
     }
     log('-----------------------------------------------')
 
@@ -70,7 +71,7 @@ global.setTimeout(()=> {
         log(`${bad[i].nickName}`)
     }
     log('-----------------------------------------------')
-}, 5000)
+}, 10000)
 
 
 
